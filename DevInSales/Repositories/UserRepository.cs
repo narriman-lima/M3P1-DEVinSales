d66c3cc27447d56f4e5ad49f6e6070b4c835240d
Narriman Lima <narrimancslima@gmail.com>
2022-06-08 10:16:53 -0300

    Feat: Cria UserRepository e método para login

---
+++ DevInSales/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevInSales.Models;
+
+namespace DevInSales.Repositories
+{
+    public class UserRepository
+    {
+        public static User VerificarUsuarioESenha(string username, string password)
+        {
+            var users = new List<User>();
+
+            users.Add(new User() { Id = 1, Name = "Paula", Password = "P123!@", Role = "funcionario" });
+            users.Add(new User() { Id = 2, Name = "Marcio", Password = "M123!@", Role = "gerente" });
+            users.Add(new User() { Id = 3, Name = "Renata", Password = "R123!@", Role = "administrador" });
+
+            return users.Where(x => x.Name.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
+
