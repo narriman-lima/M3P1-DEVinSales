ca1736ec7353f1bc2b3b795997d40739028d5d89
Narriman Lima <narrimancslima@gmail.com>
2022-06-08 20:03:46 -0300

    Fix: Altera palavra Funcionario para Usuario

--- DevInSales/Repositories/UserRepository.cs
+++ DevInSales/Repositories/UserRepository.cs
@@ -9,9 +9,9 @@ namespace DevInSales.Repositories
         {
             var users = new List<User>();
 
-            users.Add(new User() { Id = 1, Name = "Paula", Password = "P123!@", Role = "usuario", Permissao = Permissoes.Usuario });
-            users.Add(new User() { Id = 2, Name = "Marcio", Password = "M123!@", Role = "gerente", Permissao = Permissoes.Gerente });
-            users.Add(new User() { Id = 3, Name = "Renata", Password = "R123!@", Role = "administrador", Permissao = Permissoes.Administrador });
+            users.Add(new User() { Id = 1, Name = "Paula", Password = "P123!@", Role = "usuario" });
+            users.Add(new User() { Id = 2, Name = "Marcio", Password = "M123!@", Role = "gerente" });
+            users.Add(new User() { Id = 3, Name = "Renata", Password = "R123!@", Role = "administrador" });
 
             return users.Where(x => x.Name.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
         }
