9d085182a59823c7aaf3ec17645bea752e191102
Narriman Lima <narrimancslima@gmail.com>
2022-06-08 18:32:03 -0300

    Fix: adiciona summary ao endpoint da controller Authentication e melhoria no retorno

--- DevInSales/Controllers/AuthenticationController.cs
+++ DevInSales/Controllers/AuthenticationController.cs
@@ -1,10 +1,12 @@
 ﻿using DevInSales.DTOs;
+using DevInSales.Enums;
 using DevInSales.Models;
 using DevInSales.Repositories;
 using DevInSales.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Extensions;
 
 namespace DevInSales.Controllers
 {
@@ -13,6 +15,12 @@ namespace DevInSales.Controllers
     public class AuthenticationController : ControllerBase
     {
 
+        /// <summary>
+        /// Realiza login por nome do usuário e senha
+        /// </summary>
+        /// <returns>Token de Autenticação</returns>
+        /// <response code="200">Login efetuado com sucesso.</response>
+        /// <response code="404">Usuário e/ou senha incorreto(s).</response>
         [HttpPost]
         [Route("login")]
         public IActionResult Login([FromBody] LoginDTO dto)
@@ -23,7 +31,7 @@ namespace DevInSales.Controllers
 
             var token = TokenService.GenerateToken(user);
 
-            return Ok(token);
+            return Ok($"Bem-vindo(a) {dto.Username} ao sistema de funcionários DEVinSales.\nEste é seu token: \n\n" + token);
 
         }
     }
